78f5cf3231efb6f28bc909644d177e38c9fe8ba5
olaf <olmighty99@gmail.com>
2021-07-18 22:27:08 +0000

    add beego orm and model

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require github.com/beego/beego/v2 v2.0.1
 
 require (
+	github.com/astaxie/beego v1.12.3
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/smartystreets/goconvey v1.6.4
 )
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"beego-onc/models"
 	_ "beego-onc/routers"
-	"database/sql"
 	"fmt"
 	"os"
 
+	"github.com/astaxie/beego/orm"
 	beego "github.com/beego/beego/v2/server/web"
 	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/beego/beego/v2/core/config"
 )
 
 func init() {
@@ -22,22 +25,56 @@ func init() {
 	os.MkdirAll("./data/dev/", 0755)
 	os.Create("./data/dev/onc.db")
 
-	db, err := sql.Open("sqlite3", "./data/dev/onc.db")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	orm.RegisterDriver("sqlite3", orm.DRSqlite)
+	orm.RegisterDataBase("default", "sqlite3", "./data/dev/default.db")
+	orm.RegisterDataBase("oncdb", "sqlite3", "./data/dev/onc.db")
 
-	defer db.Close()
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS `customers` (`c_id` INTEGER PRIMARY KEY AUTOINCREMENT, `client_id` VARCHAR(64) NULL, `first_name` VARCHAR(255) NOT NULL, `last_name` VARCHAR(255) NOT NULL, `guid` VARCHAR(255) NULL, `created` DATETIME NULL, `type` VARCHAR(1))")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	// Need to register model in init
+	orm.RegisterModel(new(models.OncUser), new(models.OncProfile))
 }
 
 func main() {
+	// TODO config + RunSyncDb to func initDb()
+	// Load configs
+	iniconf, err := config.NewConfig("ini", "conf/app.conf")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	db_boot, err := iniconf.String("db::beego_db_bootstrap")
+	if err != nil {
+		fmt.Println(err)
+	}
+	db_debug, err := iniconf.String("db::beego_db_debug")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if db_debug == "true" {
+		orm.Debug = true
+	}
+
+	// orm.RunSyncdb needs to run at every startup
+	if db_boot == "true" {
+		name := "oncdb"
+		force := true
+		verbose := true
+		err := orm.RunSyncdb(name, force, verbose)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		name := "oncdb"
+		force := false
+		verbose := false
+		err := orm.RunSyncdb(name, force, verbose)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	// Enable cmd: orm syncdb
+	orm.RunCommand()
 
 	beego.Run()
 
