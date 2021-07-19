cb5547a0c8ae86b021a58cc33dc2e0585520f23c
olaf <olmighty99@gmail.com>
2021-07-19 07:56:14 +0000

    minor changes, update index, add routes

--- controllers/user.go
+++ controllers/user.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"beego-onc/models"
-
 	_ "github.com/mattn/go-sqlite3"
 
 	beego "github.com/beego/beego/v2/server/web"
@@ -16,17 +14,17 @@ type UserController struct {
 // @Description create users
 // @Success 200 {int} models.NewOncUser.clientId
 // @route /create [get]
-func (u *UserController) Get() {
-	// Requires 'SessionOn = true', 'SessionProvider = "memory"' (default)
-	// v := c.GetSession("asta")
-	// if v == nil {
-	// 	c.SetSession("asta", int(1))
-	// 	c.Data["num"] = 0
-	// } else {
-	// 	c.SetSession("asta", v.(int)+1)
-	// 	c.Data["num"] = v.(int)
-	// }
-	res := models.AddUser()
-	u.Data["json"] = map[string]int64{"clientId": res}
-	u.ServeJSON()
-}
+// func (u *UserController) Get() {
+// 	// Requires 'SessionOn = true', 'SessionProvider = "memory"' (default)
+// 	// v := c.GetSession("asta")
+// 	// if v == nil {
+// 	// 	c.SetSession("asta", int(1))
+// 	// 	c.Data["num"] = 0
+// 	// } else {
+// 	// 	c.SetSession("asta", v.(int)+1)
+// 	// 	c.Data["num"] = v.(int)
+// 	// }
+// 	res := models.AddUser()
+// 	u.Data["json"] = map[string]int64{"clientId": res}
+// 	u.ServeJSON()
+// }
